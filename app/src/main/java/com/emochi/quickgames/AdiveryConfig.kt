05ad7e4acf8af8d3827a487646e99620287eb4f7
Shameem <yourgithubemail@gmail.com>
2026-09-05 22:44:21 +0330

    Update project

--- app/src/main/java/com/emochi/quickgames/AdiveryConfig.kt
+++ app/src/main/java/com/emochi/quickgames/AdiveryConfig.kt
@@ -8,14 +8,14 @@ object AdiveryConfig {
     /**
      * Adivery Application ID from https://panel.adivery.com/
      */
-    const val APP_ID = "74c264e1-2b02-4751-8254-8c83e18a8031"
+    const val APP_ID = "7e27fb38-5aff-473a-998f-437b89426f66"
 
     /**
      * Placement IDs for Banner, Interstitial, and Rewarded Video Ads
      */
-    const val PLACEMENT_BANNER = "b6f4a810-1e5b-4369-b1d5-824c16a50b73"
-    const val PLACEMENT_INTERSTITIAL = "d1983c27-7756-4ca9-a86d-69e120f2b380"
-    const val PLACEMENT_REWARDED = "f2719a04-5867-4221-83d2-3bf9b109e992"
+    const val PLACEMENT_BANNER = "2f71ec44-f30a-4043-9cc1-f32347a07f8b"
+    const val PLACEMENT_INTERSTITIAL = "0045a4aa-1498-4790-9eed-6e33ac870e5f"
+    const val PLACEMENT_REWARDED = "3f97dc4d-3e09-4024-acaf-931862c03ba8"
 
     /**
      * Production setting: Disable debug logs to optimize release performance.
